3971e3dcc671e32885874624504f307874cb9d21
jhuerfano <jhuerfano@intergrupo.com>
2023-04-19 17:32:23 -0500

    patro decorador

---
+++ ExampleDecorator/ConcreteDecoratorB.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExampleDecorator
+{
+    // Decorators can execute their behavior either before or after the call to
+    // a wrapped object.
+    class ConcreteDecoratorB : Decorator
+    {
+        public ConcreteDecoratorB(Component comp) : base(comp)
+        {
+        }
+
+        public override string Operation()
+        {
+            return $"ConcreteDecoratorB({base.Operation()})";
+        }
+    }
+
+}
